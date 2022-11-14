d4fdb2d947e6a515d3129f75160fd4e4ed9e15d6
baidyprod <dima220920041@gmail.com>
2022-11-14 23:49:00 +0200

    wip

--- Homework6/ALL FUNCTIONS IN ONE FILE.py
+++ Homework6/ALL FUNCTIONS IN ONE FILE.py
@@ -15,24 +15,17 @@ def your_all(iterable, /):
 
 
 def your_zip(*iterables):
-    res = []  #Result list
-    list_not_final = []  #List which will be fullfilled with n-indexed elements from each iterable
-    b = [*iterables]  #Variable b is a list of different iterables of different types
-    a = tuple([list(word) for word in b])  #Variable "a" becomes a tuple of iterables converted to lists
-    e = tuple([list(word) for word in b])  #Variable "e" is the same as "a" but has different id (e = a[:] and e = a.copy() don't work idk why :/)
-    for item in e:  #Next three lines convert each digit in "e" list of list to a string. We need to do this because function min (3 lines lower) doesn't work without this.
-        for digit in item:
-            item[item.index(digit)] = str(digit)
-    list_of_length = []  #Next 4 lines we need to count the shortest iterable which was input
-    for item in e:
-        list_of_length.append(len(item))
-    minimum = min(list_of_length)
-    for i in range(minimum):  #"i" takes the index of item in iterable
-        for o in range(len(a)):  #"o" takes the index of iterable
-            list_not_final.append((a[o][i]))  #List_not_final is being appended by items of same index in different iterables
-        res.append(tuple(list_not_final))  #Result list is being appended by the previous list, converted into tuple
-        list_not_final = []  #List_not_final is being cleared before the next repeat of main loop
-    return res  #After all iterations our function returns the result list
+    tmp = []
+    for coll in iterables:
+        tmp.append(len(coll))
+    min_count_el_in_coll = min(tmp)
+    res = []
+    for index in range(min_count_el_in_coll):
+        tmp = []
+        for coll in iterables:
+            tmp.append(coll[index])
+        res.append(tuple(tmp))
+    return res
 
 
 
@@ -43,11 +36,11 @@ def your_range(a:int, b:int=None, c:int=None) -> list:
     range(start, stop) - default step value = 1
     range(start, stop, step)
     '''
-    if b == None and c == None:
+    if b is None and c is None:
         start = 0
         stop = a
         step = 1
-    elif c == None:
+    elif c is None:
         start = a
         stop = b
         step = 1
@@ -85,7 +78,7 @@ def your_enumerate(iterable, start=0):
 
 def your_filter(function, iterable):
     res = []
-    if function == None:
+    if function is None:
         for item in iterable:
             if bool(item):
                 res.append(item)
